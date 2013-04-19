d11c145cb5fa261d9937229b361fbee0f1d9cb60
Ka Tran <kabuky_knight@yahoo.com>
2013-04-19 14:28:51 +0800

    stock quotes

---
+++ livewallpaper/StockQuote/src/com/tran/livewallpaper/stockquote/Stock.java
@@ -0,0 +1,32 @@
+package com.tran.livewallpaper.stockquote;
+
+import java.util.Date;
+
+public class Stock {
+
+	private String symbol;
+	private double ask;
+	private int averageDailyVolume;
+	private double bid;
+	private double askRealTime;
+	private double bidRealTime;
+	private double bookValue;
+	private String percentageChange;
+	private double change;
+	private double chaneRealTime;
+	private double dividendShare;
+	private Date lastTradeDate;
+	private double earningShare;
+	private double daysLow;
+	private double daysHigh;
+	private double yearLow;
+	private double yearHigh;
+	
+	public Stock() {
+		
+	}
+	
+	public void fetchDataFromSource() {
+		
+	}
+}
